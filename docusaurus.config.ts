981b625286eb9d968470f547ff15c667f443b76d
mengtao <xinmengtao@icloud.com>
2024-11-15 22:24:21 +0800

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/mengtaoxin',
             },
           ],
         },
